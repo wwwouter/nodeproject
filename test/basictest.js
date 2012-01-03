0ffd9f7be835fdece2e9758fb090b903a167b2bf
wouterm <hebrox@gmail.com>
2012-01-03 14:19:07 +0100

    !

--- test/basictest.js
+++ test/basictest.js
@@ -8,7 +8,7 @@ exports.testSomething2 = function(test){
 
 exports.testSomethingEls = function(test){
     test.expect(1);
-    test.ok(1, "this assertion should FAIL!");
+    test.ok(1, "this assertion should FAIL!!");
     test.done();
 };
 
