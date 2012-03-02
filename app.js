1b2e0692806017e5e3f952d8b92c7004f4d8ea49
wouterm <hebrox@gmail.com>
2012-03-02 14:19:05 +0100

    made express app

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-function test(){
-    return true;
-}
+var express = require('express');
+
+var app = express.createServer();
+
+app.get('/', function(req, res) {
+  'use strict';
+  res.send('Hello World Domination');
+});
 
 
+app.listen(process.env.port || 8089);
 
-
-function test2(){
-    return true;
-}
--- test/basictest.js
+++ test/basictest.js
@@ -1,16 +1,8 @@
 
-exports.testSomething2 = function(test){
-    test.expect(1);
-    test.ok(1, "this assertion should pass");
-    test.done();
+exports.testSomething2 = function(test) {
+  'use strict';
+  test.expect(1);
+  test.ok(1, 'this assertion should pass');
+  test.done();
 };
-
-
-exports.testSomethingEls = function(test){
-    test.expect(1);
-    test.ok(1, "this assertion should FAIL!!");
-    test.done();
-
-};
-
 
